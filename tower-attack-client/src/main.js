22dc43c3154090682c8468e38103f8bf87968444
Michael Balagna <lunchmeat13@gmail.com>
2017-07-28 13:08:35 -0600

    add axios

--- tower-attack-client/src/main.js
+++ tower-attack-client/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import App from './App'
 import {routes} from './routes.js'
 
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
   routes,
